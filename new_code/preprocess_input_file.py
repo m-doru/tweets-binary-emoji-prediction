05cbf51feb32fbabd508c047faf38ed748a75995
Cip <b.cip11@gmail.com>
2017-12-06 15:12:06 +0100

    Preprocessed full file

--- new_code/preprocess_input_file.py
+++ new_code/preprocess_input_file.py
@@ -1,15 +1,24 @@
 import os
 
-FILES_AND_LABELS = {"train_pos.txt" : '1', 'train_neg.txt' : '0'}
-TRAIN_FILE = 'train_total.txt'
-LABEL_IDENTIFIER = '__label__'
+FILES_AND_LABELS = {"train_pos_full.txt" : '1', 'train_neg_full.txt' : '0'}
+INPUT_TEST_FILE = os.path.join('..', "data", 'twitter-datasets', 'test_data.txt')
 
-with open(os.path.join('processed_data', TRAIN_FILE), 'w') as f:
+OUTPUT_TRAIN_FILE = 'train_total.txt'
+OUTPUT_TEST_FILE = 'test_total.txt'
+LABEL_IDENTIFIER = ' __label__'
+
+with open(os.path.join('processed_data', OUTPUT_TRAIN_FILE), 'w') as f:
     for filename, label in FILES_AND_LABELS.items():
 
         with open(os.path.join('..', "data", 'twitter-datasets', filename), 'r') as g:
             for line in g:
                 f.write(line.strip() + LABEL_IDENTIFIER + label + '\n')
+
+with open(os.path.join('processed_data', OUTPUT_TEST_FILE), 'w') as f:
+    with open(INPUT_TEST_FILE, 'r') as g:
+        for line in g:
+            f.write(line.strip().split(',')[1] + '\n')
+
 
 
 
--- new_code/try_fastText.py
+++ new_code/try_fastText.py
@@ -4,4 +4,4 @@ import os
 TRAINING_SET = os.path.join('processed_data', 'train_total.txt')
 OUTPUT_MODEL = os.path.join('processed_data', 'model')
 
-train_supervised(input=TRAINING_SET, output='', lr=0.1, dim=100, ws=5, epoch=5, minCount=2, minCountLabel=0, minn=0, maxn=0, neg=5, wordNgrams=2, loss='softmax', bucket=2000000, thread=12, lrUpdateRate=100, t=0.0001, verbose=2, saveOutput=1)
+train_supervised(input=TRAINING_SET, output=OUTPUT_MODEL, lr=0.1, dim=100, ws=5, epoch=5, minCount=2, minCountLabel=0, minn=0, maxn=0, neg=5, wordNgrams=2, loss='softmax', bucket=2000000, thread=12, lrUpdateRate=100, t=0.0001, verbose=2, saveOutput=1)
