415e02b9316ef974afdb40fe85e9425e1cdd751c
Cip <b.cip11@gmail.com>
2017-12-06 16:23:37 +0100

    Created Wrapper for fastText and cross-validation util

--- new_code/preprocess_input_file.py
+++ new_code/preprocess_input_file.py
@@ -1,18 +1,41 @@
 import os
+import random
 
-FILES_AND_LABELS = {"train_pos_full.txt" : '1', 'train_neg_full.txt' : '0'}
+FILES_AND_LABELS_FULL = {"train_pos_full.txt" : '1', 'train_neg_full.txt' : '0'}
+FILES_AND_LABELS_PARTIAL = {"train_pos.txt" : '1', 'train_neg.txt' : '0'}
 INPUT_TEST_FILE = os.path.join('..', "data", 'twitter-datasets', 'test_data.txt')
 
-OUTPUT_TRAIN_FILE = 'train_total.txt'
+OUTPUT_TRAIN_FILE_FULL = 'train_total_full.txt'
+OUTPUT_TRAIN_FILE_PARTIAL = 'train_total_partial.txt'
 OUTPUT_TEST_FILE = 'test_total.txt'
 LABEL_IDENTIFIER = ' __label__'
 
-with open(os.path.join('processed_data', OUTPUT_TRAIN_FILE), 'w') as f:
-    for filename, label in FILES_AND_LABELS.items():
+def shuffle_inputs(filename):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+
+    lines_shuffled = random.shuffle(lines)
+
+    with open(filename, 'w') as f:
+        f.writelines(lines_shuffled)
+
+with open(os.path.join('processed_data', OUTPUT_TRAIN_FILE_FULL), 'w') as f:
+    for filename, label in FILES_AND_LABELS_FULL.items():
 
         with open(os.path.join('..', "data", 'twitter-datasets', filename), 'r') as g:
             for line in g:
                 f.write(line.strip() + LABEL_IDENTIFIER + label + '\n')
+
+
+with open(os.path.join('processed_data', OUTPUT_TRAIN_FILE_PARTIAL), 'w') as f:
+    for filename, label in FILES_AND_LABELS_PARTIAL.items():
+
+        with open(os.path.join('..', "data", 'twitter-datasets', filename), 'r') as g:
+            for line in g:
+                f.write(line.strip() + LABEL_IDENTIFIER + label + '\n')
+
+shuffle_inputs(os.path.join(os.path.join('processed_data', OUTPUT_TRAIN_FILE_FULL)))
+shuffle_inputs(os.path.join(os.path.join('processed_data', OUTPUT_TRAIN_FILE_PARTIAL)))
 
 with open(os.path.join('processed_data', OUTPUT_TEST_FILE), 'w') as f:
     with open(INPUT_TEST_FILE, 'r') as g:
--- new_code/try_fastText.py
+++ new_code/try_fastText.py
@@ -1,7 +1,92 @@
 from fastText import train_supervised
+from sklearn.model_selection import cross_val_score
+import numpy as np
+import logging
 import os
 
-TRAINING_SET = os.path.join('processed_data', 'train_total.txt')
-OUTPUT_MODEL = os.path.join('processed_data', 'model')
+logging.basicConfig(filename='app_log.log',level=logging.INFO)
 
-train_supervised(input=TRAINING_SET, output=OUTPUT_MODEL, lr=0.1, dim=100, ws=5, epoch=5, minCount=2, minCountLabel=0, minn=0, maxn=0, neg=5, wordNgrams=2, loss='softmax', bucket=2000000, thread=12, lrUpdateRate=100, t=0.0001, verbose=2, saveOutput=1)
+TRAINING_SET = os.path.join('processed_data', 'train_total_full.txt')
+
+SWAP_FILE = os.path.join('processed_data', 'swap_file.txt')
+LABEL_IDENTIFIER = ' __label__'
+
+class FastTextClassifier:
+    def __init__(self, params):
+        self.model = None
+        self.params = params
+
+    def fit(self, X, y):
+        with open(SWAP_FILE, 'w') as f:
+            for tweet, label in zip(X, y):
+                f.write(tweet + LABEL_IDENTIFIER + label + '\n')
+
+        self.model = train_supervised(SWAP_FILE, **self.params)
+        os.remove(SWAP_FILE) # DELETE SWAP FILE AFTER USAGE
+
+    def predict(self, X):
+        result = []
+
+        for tweet in X:
+            prediction = self.model.predict(tweet, 1)[0][0]
+            result.append(prediction)
+
+        return np.array(result)
+
+    def score(self, X, y):
+        predicted = np.array([self.predict(tweet) for tweet in X])
+        return np.count_nonzero(predicted == y)
+
+def construct_dataset_from_file(filename):
+    X = []
+    y = []
+
+    with open(filename, 'r') as f:
+        for line in f:
+            tweet, label = line.strip().split(LABEL_IDENTIFIER)
+            X.append(tweet)
+            y.append(label)
+
+    X = np.array(X)
+    y = np.array(y)
+
+    X = np.random.shuffle(X)
+    y = np.random.shuffle(y)
+
+    return X, y
+
+parameters = {'lr':0.1,
+          'dim':100,
+          'ws':5,
+          'epoch':5,
+          'minCount':2,
+          'minCountLabel':0,
+          'minn':0,
+          'maxn':0,
+          'neg':5,
+          'wordNgrams':2,
+          'loss':'softmax',
+          'bucket':2000000,
+          'thread':12,
+          'lrUpdateRate':100,
+          't':0.0001,
+          'verbose':2}
+
+logging.info("################################################################")
+
+X, y = construct_dataset_from_file(TRAINING_SET)
+print("Finished constructing dataset.")
+logging.info("Finished constructing dataset.")
+
+classifier = FastTextClassifier(parameters)
+
+scores = cross_val_score(classifier, X, y, cv=5)
+logging.info("Parameters: " + str(parameters))
+logging.info("Mean accuracy: " + str(scores.mean()))
+logging.info("Scores detailed: " + str(scores))
+
+print("Mean accuracy: " + str(scores.mean()))
+print("Scores detailed: " + str(scores))
+
+
+
