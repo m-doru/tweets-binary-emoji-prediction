d71ddba377b501e480461bbbb9aadecb80803900
Doru Musuroi <doru.musuroi@gmail.com>
2017-12-09 18:38:09 +0100

    More work on the normalization script

--- twitter-datasets/process_data.sh
+++ twitter-datasets/process_data.sh
@@ -21,11 +21,36 @@ for i in {1..6}; do
     sed -i -e "s/${pattern}/${repl_pat}/g" $file
 done
 
+# create one token from multiple open brackets
 pattern=") ) ) ) ) ) ) ) ) ) ) ) ) )"
 repl_pat="))))))))))))))"
 
 
 for i in {1..11}; do
+    pattern=${pattern:2}
+    repl_pat=${repl_pat:1}
+    echo $repl_pat
+
+    sed -i -e "s/${pattern}/${repl_pat}/g" $file
+done
+
+# create one token from multiple exclamation marks
+pattern="! ! ! ! ! ! ! ! ! !"
+repl_pat="!!!!!!!!!!"
+
+for i in {1..8}; do
+    pattern=${pattern:2}
+    repl_pat=${repl_pat:1}
+    echo $repl_pat
+
+    sed -i -e "s/${pattern}/${repl_pat}/g" $file
+done
+
+# create one token from multiple question marks
+pattern="? ? ? ? ? ? ?"
+repl_pat="???????"
+
+for i in {1..5}; do
     pattern=${pattern:2}
     repl_pat=${repl_pat:1}
     echo $repl_pat
